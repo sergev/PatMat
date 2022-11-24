088ce4ddc7382ebe1b7233c7f24042a655e03161
Serge Vakulenko <serge.vakulenko@gmail.com>
2022-11-24 01:02:46 -0800

    Add examples from the book.

---
+++ Test/example1.C
@@ -0,0 +1,17 @@
+#include "Pattern.H"
+
+using namespace PatMat;
+using namespace std;
+
+int main()
+{
+    // Example 1 from The Snobol4 book, section 2.16.1
+    string x;
+
+    const Pattern pair = (Len(1) % x & +x) * output;
+
+    pair("cook"); cout << endl;
+    pair("common"); cout << endl;
+    pair("aaron"); cout << endl;
+    pair("chickadee"); cout << endl;
+}
---
+++ Test/example4.C
@@ -0,0 +1,37 @@
+#include "Pattern.H"
+
+using namespace PatMat;
+using namespace std;
+
+void match(const Pattern &exp, const std::string &input)
+{
+    // Must match the whole string.
+    const Pattern pattern = Pos(0U) & exp & Rpos(0U);
+
+    if (pattern(input)) {
+        cout << input << " is an expression" << endl;
+    } else {
+        cout << input << " is not an expression" << endl;
+    }
+}
+
+int main()
+{
+    // Example 4 from The Snobol4 book, section 2.16.4.
+    // Enhanced to avoid recursion in term and exp.
+
+    Pattern variable, addop, mulop, factor, term, exp;
+
+    variable = Any("xyz");
+    addop    = Any("+-");
+    mulop    = Any("*/");
+    factor   = variable | '(' & +exp & ')';
+    term     = factor & Arbno(mulop & factor);
+    exp      = addop & term & Arbno(addop & term) | term & Arbno(addop & term);
+
+    match(exp, "x+y*(z+x)");
+    match(exp, "x+y+z");
+    match(exp, "xy");
+    match(exp, "-x*y");
+    match(exp, "+x*-y");
+}
